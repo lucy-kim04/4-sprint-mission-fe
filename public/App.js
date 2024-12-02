27fd8c657b5ef945644670ee97d909f52e894a86
HJ <a01025105259@gmail.com>
2024-12-02 10:42:07 +0900

    app.js 완성

--- public/App.js
+++ public/App.js
@@ -1,3 +1,18 @@
 const BASE_URL = "https://panda-market-api.vercel.app";
 
-export const getProducts = async
+export const getProducts = async ({
+  page = 1,
+  pageSize = 4,
+  orderBy = "",
+  keyword = "",
+}) => {
+  const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
+  const res = await fetch(`${BASE_URL}/products?${query}`);
+
+  if (!res.ok) {
+    throw new Error("상품 목록 조회에 실패하였습니다");
+  }
+  const data = await res.json();
+
+  return data;
+};
